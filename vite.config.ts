898d027a94b24047ee1120549d315bdeaf0367cf
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-02 20:15:42 -0600

    URL's base

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -6,6 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@' : fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })

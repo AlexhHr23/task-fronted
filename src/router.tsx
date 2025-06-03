158d111855b16c0400f101b51049c94b5d19860f
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-02 20:35:56 -0600

    Creando paguna para crear proyecto

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "@/layouts/AppLayout";
 import { DashboardView } from "@/views/DashboardView";
+import { CreateProjectView } from "./views/projects/CreateProjectView";
 
 
 export const Router = () => {
@@ -9,6 +10,7 @@ export const Router = () => {
             <Routes>
                 <Route element={<AppLayout/>}>   
                     <Route path="/" element={<DashboardView/>} index />
+                    <Route path="/create/project" element={<CreateProjectView/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom"
 
-
-export const DashboardView = () => {
+export const CreateProjectView = () => {
   return (
     <>
-      <h1 className="text-5xl font-black">Mis proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+      <h1 className="text-5xl font-black">Crear proyecto</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para crear proyecto</p>
 
       <nav className="my-5">
       <Link
-        to='/create/project' 
+        to='/' 
         className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors">
-          Nuevo proyecto
+          Volver a proyectos
       </Link>
       </nav>
       

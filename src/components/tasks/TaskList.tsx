fd5abb3cbc63a518d9a3ad4f263c1bde8aefb511
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-12 21:35:02 -0600

    Colores de los estados

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -25,6 +25,15 @@ const statusTranslations  : {[key: string] : string} = {
   completed: 'Completado'
 }
 
+const statusStyles  : {[key: string] : string} = {
+  pending: 'border-t-slate-500',
+  onHold: 'border-t-red-500',
+  inProgress: 'border-t-blue-500',
+  underReview: 'border-t-amber-500',
+  completed: 'border-t-emerald-500'
+}
+
+
 export const TaskList = ({ tasks }: taskListProps) => {
 
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -32,8 +41,6 @@ export const TaskList = ({ tasks }: taskListProps) => {
     currentGroup = [...currentGroup, task]
     return { ...acc, [task.status]: currentGroup };
   }, initialStatusGroups);
-
-  console.log(groupedTasks);
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
@@ -43,7 +50,7 @@ export const TaskList = ({ tasks }: taskListProps) => {
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
 
             <h3 
-            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}
+            className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
             >{statusTranslations[status]}</h3>
 
             <ul className='mt-5 space-y-5'>

7044cebe486c0d1d3ddc31fba242810e22e31bc7
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-17 17:48:07 -0600

    Registrar cuenta

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from "react-toastify"
 import { Logo } from "@/components/Logo"
 import { Outlet } from "react-router-dom"
 
@@ -7,12 +8,16 @@ export const AuthLayout = () => {
         <>
             <div className="bg-gray-800 min-h-screen">
                 <div className="py-10 lg:py-20 mx-auto w-[450px]">
-                    <Logo/>
+                    <Logo />
                     <div className="mt-10">
                         <Outlet />
                     </div>
                 </div>
             </div>
+            <ToastContainer
+                pauseOnHover={false}
+                pauseOnFocusLoss={false}
+            />
         </>
     )
 }

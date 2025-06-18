c7aab48190ee14a61d12bab0e9f09561c22d543d
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-17 21:25:33 -0600

    Creando función de autenticar

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import type { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
+import type { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 export const createAccount = async(formData: UserRegistrationForm) =>  {
     try {
@@ -30,6 +30,18 @@ export const confirmAccount = async(tokenData: ConfirmToken) => {
 export const requestConfirmation = async(formData: RequestConfirmationCodeForm) => {
     try {
         const url = '/auth/request-code'
+        const {data} = await api.post(url, formData)
+        return data
+    } catch (error) {
+         if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export const login = async(formData: UserLoginForm) => {
+    try {
+        const url = '/auth/login'
         const {data} = await api.post(url, formData)
         return data
     } catch (error) {

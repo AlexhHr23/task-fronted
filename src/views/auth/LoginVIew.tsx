e5c6f31eb1a2b669f40341e61485fb3ccfd1f650
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-06-19 17:54:19 -0600

    Redirecciones  al iniciar sesión

--- src/views/auth/LoginVIew.tsx
+++ src/views/auth/LoginVIew.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import type { UserLoginForm } from "@/types/index";
 import { ErrorMessage } from "@/components/ErrorMessage";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { login } from "@/api/AuthAPI";
 import { toast } from "react-toastify";
@@ -14,13 +14,16 @@ export const LoginView = () => {
   }
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
+  const navigate = useNavigate()
+
   const { mutate } = useMutation({
     mutationFn: login,
     onError: (error) => {
       toast.error(error.message)
     },
-    onSuccess: (data) => {
-      toast.success('Iniciando sesión...')
+    onSuccess: () => {
+      navigate('/')
+
     }
   })
 

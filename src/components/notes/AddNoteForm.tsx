065737ba5e890e9d92678f3d20b0c4446d058982
AlexhHr23 <alex.hernandez24kb@gmail.com>
2025-08-22 17:35:13 -0600

    crear formulario de nota

---
+++ src/components/notes/AddNoteForm.tsx
@@ -0,0 +1,24 @@
+
+export const AddNoteForm = () => {
+  return (
+    <form
+        onSubmit={() =>  {}}
+        className="space-y-3"
+        noValidate
+    >
+        <div className="flex flex-col gap-2">
+            <label className="font-bold" htmlFor="content">Crear Nota</label>
+            <input 
+                id="content"
+                type="content"
+                placeholder="Contenido de la nota" 
+                className="w-full p-3 border border-gray-300"
+            />
+            <input type="submit" 
+                value="Crear nota"
+                className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-2 text-white font-black curson-pointer"
+            />
+        </div>
+    </form>
+  )
+}

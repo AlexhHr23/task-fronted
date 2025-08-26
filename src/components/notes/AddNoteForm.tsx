d5f66cdc767626c7e06e099c1808e84bab7fa102
AlexhHr23 <alex.hernandez24kb@gmail.com>
2025-08-26 11:05:42 -0600

    Crear función para crear notas

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,40 +1,68 @@
 import type { NoteFormData } from '@/types/index'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { ErrorMessage } from '../ErrorMessage'
-
-const initialValues :  NoteFormData = {
-    content: ''
-}
-
-const {register, handleSubmit, formState:{errors} } = useForm({defaultValues: initialValues})
-
+import { useMutation } from '@tanstack/react-query'
+import { createNote } from '@/api/NoteAPI'
+import { toast } from 'react-toastify'
+import { useLocation, useParams } from 'react-router-dom'
+import { CLIENT_RENEG_WINDOW } from 'tls'
 
 export const AddNoteForm = () => {
-  return (
-    <form
-        onSubmit={() =>  {}}
-        className="space-y-3"
-        noValidate
-    >
-        <div className="flex flex-col gap-2">
-            <label className="font-bold" htmlFor="content">Crear Nota</label>
-            <input 
-                id="content"
-                type="content"
-                placeholder="Contenido de la nota" 
-                className="w-full p-3 border border-gray-300"
-                {...register('content', {
-                    required: 'El contenido de la nota es obligatorio'
-                })}
-            />
-            {errors.content && (
-                <ErrorMessage>{errors.content.message}</ErrorMessage>
-            )}
-            <input type="submit" 
-                value="Crear nota"
-                className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-2 text-white font-black curson-pointer"
-            />
-        </div>
-    </form>
-  )
+
+    const params = useParams()
+    const location = useLocation()
+
+    const queryParams = new URLSearchParams(location.search)
+
+    const projectId = params.projectId!
+    const taskId = queryParams.get('viewTask')!
+
+    const initialValues: NoteFormData = {
+        content: ''
+    }
+
+    const { register, handleSubmit, reset,  formState: { errors } } = useForm({ defaultValues: initialValues })
+
+    const  {mutate} = useMutation({
+        mutationFn: createNote,
+        onError: (error) =>  {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    })
+
+    const handleAddNote = (formData: NoteFormData) => {
+        mutate({projectId, taskId, formData})
+        reset()
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit(handleAddNote)}
+            className="space-y-3"
+            noValidate
+        >
+            <div className="flex flex-col gap-2">
+                <label className="font-bold" htmlFor="content">Crear Nota</label>
+                <input
+                    id="content"
+                    type="content"
+                    placeholder="Contenido de la nota"
+                    className="w-full p-3 border border-gray-300"
+                    {...register('content', {
+                        required: 'El contenido de la nota es obligatorio'
+                    })}
+                />
+                {errors.content && (
+                    <ErrorMessage>{errors.content.message}</ErrorMessage>
+                )}
+                <input type="submit"
+                    value="Crear nota"
+                    className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-2 text-white font-black curson-pointer"
+                />
+            </div>
+        </form>
+    )
 }

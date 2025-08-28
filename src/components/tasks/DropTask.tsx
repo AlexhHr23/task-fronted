abe9b8db388c6f0806c9ee91fbe1c0d232875e08
AlexhHr23 <alex.hernandez24kb@gmail.com>
2025-08-28 13:22:06 -0600

    Añadiendo efecto drag

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -1,20 +1,26 @@
 import { useDroppable } from "@dnd-kit/core"
 
 type DropTaskProps = {
-  status: string 
+  status: string
 }
 
-export const DropTask = ({status}: DropTaskProps) => {
+export const DropTask = ({ status }: DropTaskProps) => {
 
-  const {isOver, setNodeRef} = useDroppable({
+  const { isOver, setNodeRef } = useDroppable({
     id: status
   })
+
+  const style = {
+    opacity: isOver ? 0.4 : undefined
+  }
+
   return (
     <div
-    ref={setNodeRef}
+      style={style}
+      ref={setNodeRef}
       className="text-xs font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5  grid place-content-center text-slate-500"
     >
-        Soltar tarea aquí - {status}
+      Soltar tarea aquí - {status}
     </div>
   )
 }
